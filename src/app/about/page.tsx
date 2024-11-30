50c518242ae74814c64c1b02945a788f6f253fbc
Ali Suliman <alisuliman@Alis-MacBook-Pro.local>
2024-11-30 18:56:30 +0100

    feat: basic configurations

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Container, Typography } from "@mui/material";
+
+const About = () => {
+  return (
+    <Container sx={{p: 8, pb: 20}}>
+      <Typography variant="h1">About me</Typography>
+    </Container>
+  );
+}
+
+
+export default About;
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { Container, Typography } from "@mui/material";
+
+const Projects = () => {
+  return (
+    <Container sx={{p: 8, pb: 20}}>
+      <Typography variant="h1">Projects</Typography>
+    </Container>
+  )
+}
+
+export default Projects;
